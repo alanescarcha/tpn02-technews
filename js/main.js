e0ca7e8d1472e884f5c18fcdf326696ec22c4512
Alan Escarcha <63257497+alanescarcha@users.noreply.github.com>
2021-06-03 20:01:06 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('#mobile-menu').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {
+    delay: 500
+});
+ScrollReveal().reveal('.cards-banner-one', {
+    delay: 500
+});
+ScrollReveal().reveal('.cards-banner-two', {
+    delay: 500
+});
